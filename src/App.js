8f6f805aa8eddae567cf8b5b863b2c99203c8ddd
pauline <pauline.feytel@gmail.com>
2019-03-15 14:12:25 +0100

    Add an input and the length of the entered text

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    text: "",
+    lengthText: 0
+  }
+
+  lengthText = (event) => {
+    this.setState({lengthText: event.target.value.length});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <input type="text" defaultValue={this.state.text} onChange={this.lengthText}></input>
+        <p>{this.state.lengthText}</p>
       </div>
     );
   }
